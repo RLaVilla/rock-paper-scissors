b5ab8d5b4bf84791e3bdd3a119130c72bba63a11
RLaVilla <codingrlav@gmail.com>
2024-08-18 22:10:25 -0400

    Add files via upload

--- rps.js
+++ rps.js
@@ -7,6 +7,8 @@
 
     let humanScore = 0;
     let computerScore = 0;
+    let roundCount = 0;
+    const maxRounds = 5;
     
     
     function getComputerChoice() {
@@ -20,68 +22,124 @@
             return "scissors";
     }
     
-    function getHumanChoice() {
-        let humanChoice = prompt("Rock, Paper, or Scissors?");
-        humanChoice = humanChoice.toLowerCase();
-        return humanChoice;
+    function startGame () {
+        humanScore = 0;
+        computerScore = 0;
+        roundCount = 0;
+        enableButtons();
+        const logDiv = document.getElementById("log");
+        logDiv.textContent = "";
     }
+
+
+    const playbutton = document.getElementById("playbutton");
+            playbutton.addEventListener("click", () => {
+                logMessage("New game has begun. Select Rock, Paper, or Scissors to begin.")
+                startGame();
+            });
+            
+    const rbutton = document.getElementById("rbutton");
+    const pbutton = document.getElementById("pbutton");
+    const sbutton = document.getElementById("sbutton");
+    const btncontainer = document.getElementsByClassName("btncontainer");
+        
+    //     for (let i = 0; i < btncontainer.length; i++) {
+    //     btncontainer[i].addEventListener("click", () => {
+    //         logMessage("Round " + roundCount)
+    //     });
+    // }
+
+        rbutton.addEventListener("click", () => {
+            playRound("rock", getComputerChoice())
+        });
+        pbutton.addEventListener("click", () => {
+            playRound("paper", getComputerChoice()) 
+        });
+        sbutton.addEventListener("click", () => {
+            playRound("scissors", getComputerChoice()) 
+        });
+
+
+    function logMessage(message) {
+        const logDiv = document.getElementById("log");
+        const newMessage = document.createElement("p");
+        newMessage.textContent = message;
+        logDiv.appendChild(newMessage);
+        }
+    
     
         function playRound (humanChoice, computerChoice) {
-    
+            roundCount++;
+            logMessage("Round " + roundCount + ":");
 
         if (humanChoice === "rock" && computerChoice === "scissors") {   
-            humanScore += 1       
-            console.log("You won this round!");
+            humanScore += 1  
+            logMessage("You chose Rock. Computer chose Scissors. You won this round!");  
         }
+
         else if (humanChoice === "rock" && computerChoice === "paper") {
             computerScore += 1
-            console.log("You lost this round!");
+            logMessage("You chose Rock. Computer chose Paper. You lost this round!");
         }
-        else if (humanChoice === "rock" && computerChoice === "rock")
-            console.log("Tie!");
-        else if (humanChoice === "paper" && computerChoice === "scissors"){
+
+        else if (humanChoice === "rock" && computerChoice === "rock") {
+            logMessage("Both players chose Rock. Tie!");
+        }
+
+        else if (humanChoice === "paper" && computerChoice === "scissors") {
             computerScore += 1
-            console.log("You lost this round!");
+            logMessage("You chose Paper. Computer chose Scissors. You lost this round!");
         }
-        else if (humanChoice === "paper" && computerChoice === "paper") 
-            console.log("Tie!");
+
+        else if (humanChoice === "paper" && computerChoice === "paper") {
+            logMessage("Both players chose Paper. Tie!");
+        }
+
         else if (humanChoice === "paper" && computerChoice === "rock") { 
             humanScore += 1
-            console.log("You won this round!");
+            logMessage("You chose Paper. Computer chose Rock. You won this round!");
         }
-        else if (humanChoice === "scissors" && computerChoice === "scissors")
-            console.log("Tie.")
+
+        else if (humanChoice === "scissors" && computerChoice === "scissors") {
+            logMessage("Both players chose Scissors. Tie!")
+        }
+
         else if (humanChoice === "scissors" && computerChoice === "paper") {
             humanScore += 1
-            console.log("You won this round!");
+            logMessage("You chose Scissors. Computer chose Paper. You won this round!");
         }
+
         else if (humanChoice === "scissors" && computerChoice === "rock") { 
             computerScore += 1
-            console.log("You lost this round!");
+            logMessage("You chose Scissors. Computer chose Rock. You lost this round!");
         }
+
+        if (roundCount >= maxRounds) {
+            logMessage ("Game Over. Final Score - Human: " + humanScore + " Computer: " + computerScore);
+            if (humanScore > computerScore) {
+                logMessage("You won this game!")
+            }
+            else if (computerScore > humanScore) {
+                logMessage("The computer won this game!")
+            }
+            else {
+                logMessage("The score was tied. No winner.")  
+            }  
+                disableButtons();
+                return;
+        }
+    }
+
+    function disableButtons () {
+        rbutton.disabled = true;
+        pbutton.disabled = true;
+        sbutton.disabled = true;
     }
  
-    function playGame() {
-
-        for (let i = 1; i <= 5; i++){
-            const humanSelection = getHumanChoice();
-            const computerSelection = getComputerChoice();
-            console.log("Round " + i);
-            console.log("You picked " + humanSelection + ".")
-            console.log("The computer picked " + computerSelection + ".")
-            playRound(humanSelection, computerSelection)
-        }
-        if (humanScore > computerScore) {
-            return "You won!"
-        }
-        else if (computerScore > humanScore) {
-            return "The computer won! Play again."
-        }
-        else 
-            return "The score was tied. No winner."
-            
-       
+    function enableButtons () {
+        rbutton.disabled = false;
+        pbutton.disabled = false;
+        sbutton.disabled = false;
     }
     
-
-    console.log(playGame())
+        
